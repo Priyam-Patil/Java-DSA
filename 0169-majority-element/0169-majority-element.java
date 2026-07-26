ab04bf92c4d1d721b05c2e4065e3a5cf822d4b9c
Priyam Patil <patilpriyam03@gmail.com>
2026-07-26 15:01:27 +0530

    Time: 12 ms (37.19%), Space: 52.6 MB (85.54%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,20 +1,21 @@
-// Moore's voting algorithm
+import java.util.*;
 
 class Solution {
     public int majorityElement(int[] nums) {
-        int candidate=0;
-        int points=0;
-        for(int i=0;i<nums.length;i++){
-            if(points==0){
-                candidate=nums[i];
-            }
-            if(candidate==nums[i]){
-                points++;
-            }else{
-                points--;
-            }
-            
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+
         }
-        return candidate;
+
+        for (int key : map.keySet()) {
+            if (map.get(key) > nums.length / 2) {
+                return key;
+            }
+        }
+
+        return -1;
+
     }
 }
