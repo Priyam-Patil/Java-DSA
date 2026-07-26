94cbbef7807d705269c8d385f669364a09111450
Priyam Patil <patilpriyam03@gmail.com>
2026-07-26 15:01:53 +0530

    Time: 15 ms (24.19%), Space: 52.2 MB (97.32%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,3 +1,26 @@
+// Moore's voting algorithm
+// class Solution {
+//     public int majorityElement(int[] nums) {
+//         int candidate=0;
+//         int points=0;
+//         for(int i=0;i<nums.length;i++){
+//             if(points==0){
+//                 candidate=nums[i];
+//             }
+//             if(candidate==nums[i]){
+//                 points++;
+//             }else{
+//                 points--;
+//             }
+            
+//         }
+//         return candidate;
+//     }
+// }
+
+
+
+
 import java.util.*;
 
 class Solution {
