8852874a4fd9ea39a66977748b04ffc4d47ab7fd
Priyam Patil <patilpriyam03@gmail.com>
2026-08-05 21:14:28 +0530

    Time: 1066 ms (5%), Space: 0B (100%) - LeetHub

--- 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
+++ 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
@@ -6,5 +6,7 @@ sum(amount) as trans_total_amount,
 sum(case when state="approved" then amount else 0 end) as approved_total_amount
 from Transactions 
 group by month,country;
+-- for Alias name--> in execution order group by comes first before select but
+-- MySQL has a special feature where it rewrites the alias internally.
 
 
