8dc1bf1f881e32ec33ebbc4f6bbff33c14e2ef09
Priyam Patil <patilpriyam03@gmail.com>
2026-08-06 18:25:53 +0530

    Time: 685 ms (46.98%), Space: 0B (100%) - LeetHub

---
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -0,0 +1,7 @@
+
+select round (sum(case when min_o_date=min_pref_date then 1 else 0 end)/count(*)*100,2) as immediate_percentage
+from
+(select customer_id ,min(order_date) as min_o_date, min(customer_pref_delivery_date ) as min_pref_date
+from Delivery
+group by customer_id ) as derived_table;
+
