280e34581622725539a4855d7316b82d29e5d476
Priyam Patil <patilpriyam03@gmail.com>
2026-08-08 16:23:50 +0530

    Time: 745 ms (31.35%), Space: 0B (100%) - LeetHub

--- 1731-the-number-of-employees-which-report-to-each-employee/1731-the-number-of-employees-which-report-to-each-employee.sql
+++ 1731-the-number-of-employees-which-report-to-each-employee/1731-the-number-of-employees-which-report-to-each-employee.sql
@@ -1,4 +1,5 @@
-
+-- JOIN does not change the GROUP BY rule
+-- Because manager.employee_id uniquely determines manager.name
 select manager.employee_id,manager.name,count(*) as  reports_count,round(avg(emp.age)) as average_age 
 from  employees as emp
 INNER JOIN employees as manager
